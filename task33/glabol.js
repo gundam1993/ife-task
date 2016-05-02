83b5f32acd3be5dbac3472ec6dc3ed7b24d24eec
gundam1993 <gundam1993@gmail.com>
2016-05-02 20:44:10 +0800

    code refactor

--- task33/glabol.js
+++ task33/glabol.js
@@ -1,70 +1,75 @@
  $ = function (el) { return document.querySelector(el); };
  $$ = function (el) { return document.querySelectorAll(el); };
 
-var directions = ['top','right','bottom','left']
-
 function ChessboardWalker() {//小格子的原型对象
-	this.x = 4;//格子所处的横向位置
-	this.y = 4;//格子所处的纵向位置
-	this.position = '#cell44'
+	this.x = 306;//格子所处的横向位置
+	this.y = 306;//格子所处的纵向位置
+	this.position = 0;
 	this.direction = 0;//格子的方向，数值是数组directions中方向的位置
 }
 
 ChessboardWalker.prototype.show = function() {
-	$(this.position).innerHTML = '';
-	var div =document.createElement('div'),
-		position ='#cell' + this.x  + this.y;
-	div.className = directions[this.direction];//为格子增加方向信息
+	$('#moveArea').innerHTML = '';
+	var div =document.createElement('div');
+	div.style.top = this.y + 'px';
+	div.style.left = this.x +'px';
 	div.id = 'target';
-	$(position).appendChild(div);
-	this.position = position;
+	$('#moveArea').appendChild(div);
 };
 
-ChessboardWalker.prototype.go = function() {
+ChessboardWalker.prototype.go = function(target) {
 	switch (this.direction) 
 	{
 		case 0 :
-			if (this.x > 0 ) {
-				this.x --;
+			if (this.y > 62 ) {
+				this.y -= 61;
+				target.style.top = this.y + 'px';
 			};
 			break;
 		case 2 :
-			if (this.x != 9) {
-				this.x ++;
-			}
+			if (this.y < 611) {
+				this.y += 61;
+				target.style.top = this.y + 'px';
+			};
 			break;
 		case 1 :
-			if (this.y != 9) {
-				this.y ++;
-			}
+			if (this.x < 611) {
+				this.x += 61;
+				target.style.left = this.x + 'px';
+				};
 			break;
 		case 3 :
-			if (this.y != 0) {
-				this.y --;
-			}
+			if (this.x > 62) {
+				this.x -= 61;
+				target.style.left = this.x + 'px';
+				};
 			break;
 	}
-	this.show();
 };
 
-ChessboardWalker.prototype.turn = function(path) {
+ChessboardWalker.prototype.turn = function(path,target) {
 	if (path == 'right') {
+		this.position += 90;
+		target.style.transform = 'rotate(' + this.position + 'deg)';
 		this.direction ++;
 		if (this.direction > 3) {
 			this.direction -= 4;
 		};
 	}else if (path == 'left') {
+		this.position -= 90;
+		target.style.transform = 'rotate(' + this.position + 'deg)';
 		this.direction --;
 		if (this.direction < 0) {
 			this.direction += 4;
 		}
 	}else if (path == 'back') {
+		this.position += 180;
+		target.style.transform = 'rotate(' + this.position + 'deg)';
 		this.direction += 2;
 		if (this.direction > 3) {
 			this.direction -= 4;
 		};
 	}
-	this.show();
 };
 
 function runOrder() {
@@ -72,16 +77,16 @@ function runOrder() {
 		hint = $('#hint');
 		hint.style.color = 'green';
 	if (order == 'go') {
-		chessboardWalker.go();
+		chessboardWalker.go(target);
 		hint.innerHTML = 'Moved';
 	}else if (order == 'tun lef') {
-		chessboardWalker.turn('left');
+		chessboardWalker.turn('left',target);
 		hint.innerHTML = 'Turned';
 	}else if (order == 'tun rig') {
-		chessboardWalker.turn('right');
+		chessboardWalker.turn('right',target);
 		hint.innerHTML = 'Turned';
 	}else if (order == 'tun bac') {
-		chessboardWalker.turn('back');
+		chessboardWalker.turn('back',target);
 		hint.innerHTML = 'Turned';
 	}else{
 		hint.style.color = 'red';
@@ -101,19 +106,20 @@ window.onload = function () {
 }
 
 window.onclick =function () {
+	var target = $("#target");
 	switch (event.target)
 	{
 		case gobtn :
-			chessboardWalker.go();
+			chessboardWalker.go(target);
 			break;
 		case lefbtn :
-			chessboardWalker.turn('left');
+			chessboardWalker.turn('left',target);
 			break;
 		case rigbtn :
-			chessboardWalker.turn('right');
+			chessboardWalker.turn('right',target);
 			break;
 		case bacbtn :
-			chessboardWalker.turn('back');
+			chessboardWalker.turn('back',target);
 			break;
 		case run :
 			runOrder();
