75fdd1a83cc383657f9f63bff7fc25dea8f0f2c6
gundam1993 <gundam1993@gmail.com>
2016-05-21 18:29:07 +0800

    task44 finish

--- task44/waterfall.js
+++ task44/waterfall.js
@@ -1,14 +1,23 @@
-
-
+/**
+ * @constructor
+ * @param {str} selector 创建位置，querySelector格式
+ * @param {int} colunms  瀑布流栏数
+ * @param {int} margin   瀑布栏之间的外边距
+ * @param {array} pics   图片地址的数列
+ */
 var Waterfall = function (selector,colunms,margin,pics) {
     this.element = document.querySelector(selector);
     this.colunms = colunms || 4;
     this.margin = margin || 16;
     this.pics = pics;
+    this.picNum = pics.length || 0;
 
     this.init();
 };
 
+/**
+ * 创建瀑布流容器
+ */
 Waterfall.prototype.buildColunms = function() {
     this.element.className = "waterfall";
     for (var i = 0; i < this.colunms; i++) {
@@ -21,19 +30,41 @@ Waterfall.prototype.buildColunms = function() {
     }
 };
 
-Waterfall.prototype.buildPicBox = function(pic,target,i) {
+/**
+ * 创建瀑布流中每张图片的容器
+ * @param  {node} img    图片对象
+ * @param  {node} target 插入的目标栏位
+ * @param  {int} i      图片的排序
+ */
+Waterfall.prototype.buildPicBox = function(img,target,i) {
     var picbox = document.createElement("div"),
-        img = document.createElement("img"),
         content = document.createElement("div");
     content.innerHTML = "<h3>Title" + (i + 1) + "</h3>\n<p>This is a picture</p>";
     content.className = "content";
     picbox.className = "pic-box";
-    img.src = pic;
+    
+    img.addEventListener('click',function () {
+        var img = new Image(),
+            body = document.querySelector("body");
+            mask = document.querySelector("#mask");
+        mask.className = "";
+        img.src = event.target.src;
+        img.id = "bigPic";
+        body.appendChild(img);
+        setTimeout(function () {
+            var bigPic = document.querySelector("#bigPic");
+            bigPic.style.transform = 'translate(-50%, -50%)';
+        },10);
+    });
+
     picbox.appendChild(img);
     picbox.appendChild(content);
     target.appendChild(picbox);
 };
 
+/**
+ * 选出最短的瀑布流栏位
+ */
 Waterfall.prototype.colunmSort = function() {
     var colunms = document.querySelectorAll(".waterfall-colunms"),
         colunmsArr = [];
@@ -41,32 +72,81 @@ Waterfall.prototype.colunmSort = function() {
             colunmsArr.push(colunms[i]);
         }
         var sortedColunms = colunmsArr.sort(function (a,b) {
-            //console.log(a.offsetHeight);
-            //console.log(b.offsetHeight);
             return a.offsetHeight - b.offsetHeight;
         });
         return sortedColunms;
 };
 
-Waterfall.prototype.buildWaterfall = function() {
-    var target = this.colunmSort();
-        this.buildPicBox(this.pics[0],target[0],1);
+/**
+ * 创建点击图片后的遮罩层
+ */
+Waterfall.prototype.mask = function() {
+    var mask = document.createElement("div"),
+        body = document.querySelector("body");
+    mask.id = "mask";
+    mask.className = "hidden";
+    body.appendChild(mask);
+    mask.addEventListener('click',function () {
+        var bigPic = document.querySelector("#bigPic");
+        bigPic.style.transform = 'translate(-50%, -50%) scale(0.01,0.01)';
+        setTimeout(function () {
+            var bigPic = document.querySelector("#bigPic");
+            bigPic.remove();
+        },300);
+        event.target.className = "hidden";
+    });
 };
 
-
-Waterfall.prototype.popBigPic = function(event) {
-    // body...
+/**
+ * 预加载图片时回调用
+ * @param  {node} img 图片对象
+ * @param  {array} url 图片地址数组
+ * @param  {int} i   图片的排序
+ */
+Waterfall.prototype.preLoadX = function(img,url,i) {
+    var colunm = this.colunmSort(); 
+    this.buildPicBox(img,colunm[0],i);
+    if (i < url.length - 1) {
+        this.preLoad(url,i + 1);
+    }
 };
 
+/**
+ * 图片预加载，然后添加到瀑布流中
+ * @param  {array} url 图片地址数组
+ * @param  {int} i   图片的排序
+ */
+Waterfall.prototype.preLoad = function(url,i) {
+    var img = new Image();
+    img.src = url[i];
+    if (img.complete) {
+        this.preLoadX(img,url,i);
+    }else{
+        img.addEventListener("load",this.preLoadX.bind(this,img,url,i));
+    }
+};
 
+/**
+ * 滚动到预定位置是加载并添加图片
+ */
+Waterfall.prototype.scollLoad = function() {
+    var screenHeight = (document.documentElement.scrollTop || document.body.scrollTop) +(document.documentElement.clientHeight || document.body.clientHeight);
+    var colunm = this.colunmSort();
+    var containerHeight = colunm[0].offsetTop  + colunm[0].offsetHeight;
+    if (containerHeight < screenHeight) {
+        var as = randomPicLoad(10);
+        this.preLoad(as,0);
+    }
+};
+
+/**
+ * 初始化，绑定滚动时间
+ * @return {[type]} [description]
+ */
 Waterfall.prototype.init = function() {
     this.buildColunms();
-    for (var i = 0; i < this.pics.length; i++) {
-        var target = this.colunmSort();
-        //console.log(target);
-        this.buildPicBox(this.pics[i],target[0],i);
-    }
-    window.addEventListener('load',this.colunmSort.bind(this))
-};
+    this.mask();
+    this.preLoad(this.pics,0);
 
-new Waterfall("#main-block",4,16,["img/1.png","img/2.png","img/3.png","img/4.png","img/3.png","img/1.png","img/2.png","img/2.png"]);
+    window.addEventListener("scroll",this.scollLoad.bind(this));
+};
